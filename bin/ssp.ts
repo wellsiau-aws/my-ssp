41c77910232b2bbe814feb94391769ac980e9402
EC2 Default User <ec2-user@ip-172-31-60-105.us-west-2.compute.internal>
2022-01-19 00:18:00 +0000

    bootstrap argocd addon

--- bin/ssp.ts
+++ bin/ssp.ts
@@ -8,6 +8,38 @@ const app = new App();
 const account = process.env.CDK_DEFAULT_ACCOUNT;
 const region = process.env.CDK_DEFAULT_REGION;
 const env = { account, region };
+
+const repoUrl = 'https://github.com/aws-samples/ssp-eks-workloads.git'
+
+const bootstrapRepo : ssp.ApplicationRepository = {
+    repoUrl,
+    path: 'envs/dev'
+}
+
+const devBootstrapArgo = new ssp.ArgoCDAddOn({
+    bootstrapRepo: {
+        ...bootstrapRepo,
+        path: 'envs/dev'
+    },
+    adminPasswordSecretName: 'argo-admin-secret',
+});
+    
+const testBootstrapArgo = new ssp.ArgoCDAddOn({
+    bootstrapRepo: {
+        ...bootstrapRepo,
+        path: 'envs/test'
+    },
+    adminPasswordSecretName: 'argo-admin-secret',
+});   
+
+const prodBootstrapArgo = new ssp.ArgoCDAddOn({
+    bootstrapRepo: {
+        ...bootstrapRepo,
+        path: 'envs/prod'
+    },
+    adminPasswordSecretName: 'argo-admin-secret',
+});
+
 const blueprint = ssp.EksBlueprint.builder()
     .account(account) 
     .region('us-west-2')
@@ -29,13 +61,16 @@ ssp.CodePipelineStack.builder()
     .stage({
       id: 'dev',
       stackBuilder: blueprint.clone('us-west-2')
+      .addOns(devBootstrapArgo)
     })
     .stage({
       id: 'test',
       stackBuilder: blueprint.clone('us-east-1')
+      .addOns(testBootstrapArgo)
     })
     .stage({
       id: 'prod',
       stackBuilder: blueprint.clone('us-east-2')
+      .addOns(prodBootstrapArgo)
     })
     .build(app, 'my-first-stack', {env});
