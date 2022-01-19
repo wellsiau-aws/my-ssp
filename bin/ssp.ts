4e79c2ebe22aebbe8e0f1bd8ac59fd18adc1cf1a
EC2 Default User <ec2-user@ip-172-31-60-105.us-west-2.compute.internal>
2022-01-19 00:37:58 +0000

    revert

--- bin/ssp.ts
+++ bin/ssp.ts
@@ -44,7 +44,7 @@ const blueprint = ssp.EksBlueprint.builder()
     .account(account) 
     .region('us-west-2')
     .addOns(
-      new ssp.ClusterAutoScalerAddOn  
+      new ssp.ClusterAutoScalerAddOn
     )
     .teams(new TeamPlatform(String(account)), new TeamApplication('application'));
       
@@ -61,16 +61,16 @@ ssp.CodePipelineStack.builder()
     .stage({
       id: 'dev',
       stackBuilder: blueprint.clone('us-west-2')
-      .addOns(devBootstrapArgo)
+      //.addOns(devBootstrapArgo)
     })
     .stage({
       id: 'test',
       stackBuilder: blueprint.clone('us-east-1')
-      .addOns(testBootstrapArgo)
+      //.addOns(testBootstrapArgo)
     })
     .stage({
       id: 'prod',
       stackBuilder: blueprint.clone('us-east-2')
-      .addOns(prodBootstrapArgo)
+      //.addOns(prodBootstrapArgo)
     })
     .build(app, 'my-first-stack', {env});
